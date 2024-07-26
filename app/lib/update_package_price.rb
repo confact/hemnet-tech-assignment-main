b670dc89774dd082760aa776979d8aa4de56d7d3
Håkan Nylén <hakan@dun.se>
2024-07-26 18:40:05 +0200

    feat: support for Municipality in update class

--- app/lib/update_package_price.rb
+++ app/lib/update_package_price.rb
@@ -2,12 +2,17 @@
 
 class UpdatePackagePrice
   def self.call(package, new_price_cents, **options)
-    Package.transaction do
-      # Add a pricing history record
-      Price.create!(package: package, amount_cents: package.amount_cents)
+    municipality_name = options[:municipality]
+    municipality = Municipality.find_or_create_by(name: municipality_name) if municipality_name.present?
 
-      # Update the current price
-      package.update!(amount_cents: new_price_cents)
+    Package.transaction do
+      # Add a pricing history record with the current municipality
+      Price.create!(package: package, amount_cents: package.amount_cents, municipality: package.municipality)
+
+      # Update the current price and optionally the municipality
+      update_params = { amount_cents: new_price_cents }
+      update_params[:municipality] = municipality if municipality.present?
+      package.update!(update_params)
     end
   end
 end
--- spec/lib/update_package_price_spec.rb
+++ spec/lib/update_package_price_spec.rb
@@ -4,29 +4,39 @@ require "spec_helper"
 
 RSpec.describe UpdatePackagePrice do
   let(:stockholm) { Municipality.create!(name: "Stockholm") }
+  let(:goteborg) { Municipality.create!(name: "Göteborg") }
   let(:package) { Package.create!(name: "Dunderhonung", municipality: stockholm) }
 
   it "updates the current price of the provided package" do
-    UpdatePackagePrice.call(package, 200_00)
+    described_class.call(package, 200_00)
     expect(package.reload.amount_cents).to eq(200_00)
   end
 
   it "only updates the passed package price" do
-    other_package = Package.create!(name: "Farmors köttbullar", amount_cents: 100_00, municipality: stockholm)
+    other_package = Package.create!(name: "Farmors köttbullar", amount_cents: 100_00, municipality: goteborg)
 
-    expect {
-      UpdatePackagePrice.call(package, 200_00)
-    }.not_to change {
+    expect do
+      described_class.call(package, 200_00)
+    end.not_to(change do
       other_package.reload.amount_cents
-    }
+    end)
   end
 
   it "stores the old price of the provided package in its price history" do
     package.update!(amount_cents: 100_00)
 
-    UpdatePackagePrice.call(package, 200_00)
+    described_class.call(package, 200_00)
     expect(package.prices).to be_one
     price = package.prices.first
     expect(price.amount_cents).to eq(100_00)
+  end
+
+  it "stores the municipality in the price history" do
+    expect(package.municipality.name).to eq("Stockholm")
+    described_class.call(package, 200_00, municipality: "Göteborg")
+    expect(package.prices).to be_one
+    price = package.prices.first
+    expect(price.municipality.name).to eq("Stockholm")
+    expect(package.reload.municipality.name).to eq("Göteborg")
   end
 end
